6c442c4b72fa5accd92fdbfcd075322425e23149
Jeff Keating <jkeatinco@gmail.com>
2023-04-16 08:24:41 -0400

    random colors

--- egg-hunt/script.js
+++ egg-hunt/script.js
@@ -212,7 +212,28 @@ function unlockAudioContext() {
     if (typeof eggCollectSound.context !== 'undefined' && eggCollectSound.context.state === 'suspended') {
       eggCollectSound.context.resume();
     }
-  }
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeColors() {
+    // Change the background color of the website
+    document.body.style.backgroundColor = getRandomColor();
+
+    // Change the color of the eggs
+    const eggs = document.querySelectorAll('.egg');
+    eggs.forEach(egg => {
+        egg.style.backgroundColor = getRandomColor();
+    });
+}
+
   
 
 
@@ -288,3 +309,5 @@ document.addEventListener('touchstart', function unlockAudioContext() {
     }
     document.removeEventListener('touchstart', unlockAudioContext);
 });
+
+setInterval(changeColors, 500);
