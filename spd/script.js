c13186fcefe6f3b6ce5377daec91c491010d1492
Jeff Keating <jkeatinco@gmail.com>
2023-11-20 09:58:44 -0500

    spd animation and tracking

--- spd/script.js
+++ spd/script.js
@@ -3,8 +3,8 @@ let timeLeft = 60; // in seconds
 let timer;
 
 const differences = [
-    { x: 2583.00, y: 2687.00, width: 82, height: 72 },
-    { x: 2048.00, y: 2700.00, width: 67, height: 102 }
+    { x: 2583.00, y: 2687.00, width: 82, height: 72, found: false },
+    { x: 2048.00, y: 2700.00, width: 67, height: 102, found: false }
     // ... other differences
 ];
 
@@ -42,22 +42,22 @@ function checkDifference(event, imageId) {
 function isCorrectSpot(event, imageId) {
     const image = document.getElementById(imageId);
     const rect = image.getBoundingClientRect();
-
-    // Calculate scaled click coordinates
     const scaleX = image.naturalWidth / rect.width;
     const scaleY = image.naturalHeight / rect.height;
     const clickX = (event.clientX - rect.left) * scaleX;
     const clickY = (event.clientY - rect.top) * scaleY;
 
-    // Check if the click is within any of the difference areas
     for (let diff of differences) {
-        if (clickX >= diff.x && clickX <= diff.x + diff.width &&
+        if (!diff.found && clickX >= diff.x && clickX <= diff.x + diff.width &&
             clickY >= diff.y && clickY <= diff.y + diff.height) {
+            diff.found = true; // Mark as found
+            showSparkleAnimation(event.clientX, event.clientY); // Trigger animation
             return true;
         }
     }
     return false;
 }
+
 
 
 function endGame(message) {
@@ -85,6 +85,17 @@ function displayCorrectSpots() {
         });
     });
 }
+
+function showSparkleAnimation(x, y) {
+    const sparkle = document.createElement('div');
+    sparkle.className = 'sparkle';
+    sparkle.style.left = x + 'px';
+    sparkle.style.top = y + 'px';
+    document.body.appendChild(sparkle);
+
+    setTimeout(() => sparkle.remove(), 1000); // Remove after animation
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     startGame();
