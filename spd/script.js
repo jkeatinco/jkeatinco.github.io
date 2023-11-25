0f3b9a9022b6224a3f2ffe0af590a360489bdb8b
Jeff Keating <jkeatinco@gmail.com>
2023-11-25 12:20:18 -0500

    using leaflet.js now :smiley:

--- spd/script.js
+++ spd/script.js
@@ -1,140 +1,146 @@
-let originalDifferences = [{x: 1208, y: 1257}, {x: 952, y: 1234}]; // Original coordinates
-let scaledDifferences = [];
-let score = 0;
-let timeLeft = 30; // Timer set for 30 seconds
+// Global Variables
+var timer = null;
+var score = 0;
+var differencesFound = 0; // You'll need to define the total number of differences
+var totalDifferences = 5; // Example value
+var map1, map2; // Declare maps globally to access them in functions
 
-// Function to calculate and apply scale factor
-function calculateScaleFactor() {
-    let gameImage = document.getElementById('image1'); // Adjust this to your game image ID
-    let naturalWidth = gameImage.naturalWidth;
-    let displayedWidth = gameImage.offsetWidth;
-    let scaleFactor = displayedWidth / naturalWidth;
+// Assuming you have the coordinates of the differences
+var differences = [
+    { id: 1, coords: [1186, 1231] },
+    { id: 2, coords: [931, 1208] },
+    // ... other differences
+];
 
-    console.log(`Natural Width: ${naturalWidth}, Displayed Width: ${displayedWidth}`);
-    console.log(`Scale Factor: ${scaleFactor}`);
-
-    scaledDifferences = originalDifferences.map(diff => {
-        return {x: diff.x * scaleFactor, y: diff.y * scaleFactor};
-    });
-    console.log(`Scaled Differences: `, scaledDifferences);
-
-    // Clear existing circles
-    document.querySelectorAll('.difference-marker').forEach(marker => marker.remove());
-
-    scaledDifferences.forEach(diff => {
-        createDifferenceMarker(diff);
-    });
-
+// Utility Functions
+function formatTime(ms) {
+    var seconds = Math.floor((ms / 1000) % 60);
+    var minutes = Math.floor((ms / (1000 * 60)) % 60);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
-function createDifferenceMarker(diff) {
-    let marker = document.createElement('div');
-    marker.className = 'difference-marker';
-    marker.style.left = `${diff.x}px`;
-    marker.style.top = `${diff.y}px`;
-    document.getElementById('container2').appendChild(marker);
+// Game-Specific Functions
+function startTimer() {
+    var startTime = new Date();
+    timer = setInterval(function() {
+        var elapsedTime = new Date() - startTime;
+        document.getElementById('time').textContent = formatTime(elapsedTime);
+    }, 1000);
 }
 
+function updateScore(points) {
+    score += points;
+    document.getElementById('score-value').textContent = score;
+}
 
-// Start the game and setup event listeners
-document.addEventListener('DOMContentLoaded', () => {
-    let gameImage = document.getElementById('image1');
-    if (gameImage.complete) {
-        calculateScaleFactor(); // The image is already loaded, calculate scale factor immediately
-    } else {
-        gameImage.onload = calculateScaleFactor; // The image is not yet loaded, wait for the load event
-    }
-
-    setInterval(updateTimer, 1000); // Update the timer every second
-    document.getElementById('image2').addEventListener('click', checkDifference); // Adjust this to your clickable game image ID
-
-
-
-    //Image maginification
-    const magnifier = document.createElement('div');
-    const magnificationFactor = 2; // Adjust the zoom level
-
-    magnifier.id = 'magnifier';
-    document.body.appendChild(magnifier); // Append to body to avoid overflow issues
-
-    document.querySelectorAll('.game-image').forEach(image => {
-        image.addEventListener('touchstart', function(e) {
-            magnifier.style.display = 'block'; // Show magnifier
-            updateMagnifier(e, image);
-        });
-
-        image.addEventListener('touchmove', e => updateMagnifier(e, image));
-
-        image.addEventListener('touchend', function() {
-            magnifier.style.display = 'none'; // Hide magnifier
-        });
-    });
-
-    function updateMagnifier(e, image) {
-        const touchX = e.touches[0].clientX;
-        const touchY = e.touches[0].clientY;
-        const imageRect = image.getBoundingClientRect();
-
-        magnifier.style.left = (touchX - magnifier.offsetWidth / 2) + 'px';
-        magnifier.style.top = (touchY - magnifier.offsetHeight / 2) + 'px';
-        magnifier.style.backgroundImage = `url('${image.src}')`;
-        magnifier.style.backgroundSize = `${imageRect.width * magnificationFactor}px ${imageRect.height * magnificationFactor}px`;
-        magnifier.style.backgroundPosition = `-${(touchX - imageRect.left) * magnificationFactor - magnifier.offsetWidth / 2}px -${(touchY - imageRect.top) * magnificationFactor - magnifier.offsetHeight / 2}px`;
-    }
-});
-
-// Function to update the timer
-function updateTimer() {
-    if (timeLeft > 0) {
-        timeLeft--;
-        document.getElementById('timer').innerText = `Time Left: ${timeLeft}s`;
-    } else {
-        document.getElementById('timer').innerText = 'Time is up! Game Over!';
+function differenceFound() {
+    differencesFound++;
+    updateScore(10); // Increment the score by 10, for example
+    if(differencesFound === totalDifferences) {
         endGame();
     }
 }
 
-
-
-
-
-function checkDifference(event) {
-    const clickX = event.offsetX;
-    const clickY = event.offsetY;
-    const tolerance = 5; // Tolerance in pixels
-
-    console.log(clickX);
-    console.log(clickY);
-
-    for (let i = 0; i < scaledDifferences.length; i++) {
-        const diff = scaledDifferences[i];
-
-        // Check if the click coordinates are within the tolerance of the difference coordinates
-        if (clickX >= (diff.x - tolerance) && clickX <= (diff.x + tolerance) &&
-            clickY >= (diff.y - tolerance) && clickY <= (diff.y + tolerance)) {
-            console.log('Difference found!');
-            score += 10;
-            document.getElementById('score').innerText = `Score: ${score}`;
-            scaledDifferences.splice(i, 1); // Remove found difference
-            break; // Exit the loop after finding a valid click
-        }
-    }
-
-    if (scaledDifferences.length === 0) {
-        console.log('All differences found! Game won!');
-        endGame();
-    }
-}
-
-
-
-
-
-
-// Function to handle the end of the game
 function endGame() {
-    console.log(`Game Over! Your score: ${score}`);
-    // Here, you can display the score and offer options to restart or exit
+    clearInterval(timer);
+    alert("Congratulations! You've found all differences!");
 }
 
-// Add more functions as needed for game features
+function resetGame() {
+    clearInterval(timer);
+    document.getElementById('time').textContent = "00:00";
+    score = 0;
+    differencesFound = 0;
+    updateScore(0);
+    // Reset the maps and clear differences
+}
+
+// Leaflet Map Related Functions
+// Function to add differences to the map
+function addDifferencesToMap(map) {
+    differences.forEach(function(difference) {
+        var marker = L.marker(map.unproject(difference.coords, map.getMaxZoom()), {
+            icon: L.divIcon({ className: 'difference-marker' }) // An invisible icon
+        });
+        
+        marker.on('click', function() {
+            // Call this when the difference is clicked
+            differenceFound();
+            marker.remove(); // Remove the marker or hide it
+        });
+        
+        marker.addTo(map);
+    });
+}
+
+function zoomIn() {
+    map1.zoomIn();
+    map2.zoomIn();
+}
+
+function zoomOut() {
+    map1.zoomOut();
+    map2.zoomOut();
+}
+
+// DOMContentLoaded event ensures the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Initialize the maps and add event listeners
+    // Maps initialization code...
+    // Initialize the first map
+    var map1 = L.map('image-container-1', {
+        center: [0, 0],
+        zoom: 1,
+        minZoom: 1,
+        maxZoom: 4,
+        crs: L.CRS.Simple,
+        zoomControl: false, // We will add custom zoom controls later
+        attributionControl: false // Hide the default attribution
+    });
+
+    // Initialize the second map
+    var map2 = L.map('image-container-2', {
+        center: [0, 0],
+        zoom: 1,
+        minZoom: 1,
+        maxZoom: 4,
+        crs: L.CRS.Simple,
+        zoomControl: false, // We will add custom zoom controls later
+        attributionControl: false // Hide the default attribution
+    });
+
+    // Assuming you have a dimensions variable for your images
+    var dimensions = [1000, 1000]; // Replace with your image dimensions
+    var bounds1 = new L.LatLngBounds(
+        map1.unproject([0, dimensions[1]], map1.getMaxZoom()),
+        map1.unproject([dimensions[0], 0], map1.getMaxZoom())
+    );
+    var bounds2 = new L.LatLngBounds(
+        map2.unproject([0, dimensions[1]], map2.getMaxZoom()),
+        map2.unproject([dimensions[0], 0], map2.getMaxZoom())
+    );
+
+    // Add the images to the maps
+    L.imageOverlay('image1.jpg', bounds1).addTo(map1);
+    L.imageOverlay('image2.jpg', bounds2).addTo(map2);
+
+    // Sync the maps to each other for scrolling and zooming
+    map1.sync(map2);
+    map2.sync(map1);
+    
+    // Add differences to maps
+    addDifferencesToMap(map1);
+    addDifferencesToMap(map2);
+    
+    // Event listeners for game controls
+    document.getElementById('start-game').addEventListener('click', function() {
+        resetGame();
+        startTimer();
+        updateScore(0);
+        // Additional logic for starting the game
+    });
+
+    document.getElementById('reset-game').addEventListener('click', resetGame);
+    document.getElementById('zoom-in').addEventListener('click', zoomIn);
+    document.getElementById('zoom-out').addEventListener('click', zoomOut);
+});
